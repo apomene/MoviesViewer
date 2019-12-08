262b731e979520da6ddd1529d8ab449315c19fa4
Apo Mene <apomene@yahoo.gr>
2019-12-08 15:10:02 +0200

    Removed dataTables files from solution

--- WebApplication1/Scripts/Main.js
+++ WebApplication1/Scripts/Main.js
@@ -1,28 +1,6 @@
 ﻿'use strict'
 
 $(document).ready(function () {
-
-    $('#MoviesTable').DataTable(
-        {
-            "columns": [
-                { "visible": false},
-                { "visible": false },
-                { "visible": false },
-                null
-            ],
-            //"lengthMenu": [[20, 50, 100, -1], [20, 50, 100, "All"]],
-            "dom": 'lBfrtip',
-            "select": {
-                style: 'multi'
-            },
-            "autoWidth": true,
-            "scrollX": true,
-            responsive: true
-        }
-    );
-    $(function () {
-        $('[rel=tooltip]').tooltip({ trigger: "hover" });
-    });  
     loadInitImage();   
 });
 
@@ -44,39 +22,3 @@ function loadImage(id) {
     var title = document.getElementById(selectedTitle).textContent;
     $('#movieTitle').html(title);
 }
-
-
-
-
-
-$('#next').click(function () {
-    var num = parseInt($('#pageNum').text());
-    var nextPage = num++;
-    //$('#pageNum').text(nextPage);
-    loadTable(nextPage);
-});
-
-function loadTable(pageNum) {  //load Movies DataTable   
-
-    var url = "/Home/GetMovies";
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: { "page": pageNum},
-        dataType: 'json',
-        success: function (data) {
-            $('#MoviesTable').DataTable().clear();
-            if (data.length > 0) {
-                for (var i in data) {
-                    $('#MoviesTable').DataTable().row.add(data[i]);
-                }
-            }
-            $('#MoviesTable').DataTable().draw();
-            $('#pageNum').text(pageNum);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            alert('eror: \xa0\xa0' + thrownError);
-        }        
-    });
-}
-
